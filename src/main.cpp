a03e4060f95e38116b8dd28e2ccb7369a2cf78b1
lakinsm <steven.m.lakin@gmail.com>
2020-10-14 15:56:54 -0600

    mixed precision

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@
 int main() {
     HostUtils utils;
     int sm61_gpu_idx = 2;  // 1080Ti, Pascal CUDA 6.1
-    int sm75_gpu_idx = 0;  // 2080Ti, Turing CUDA 7.5
+    int sm75_gpu_idx = 0;  // 2080Ti, Turing CUDA 7.5 with Tensor cores
     int full_dim = 3000;
     int half_dim = full_dim;
 
@@ -33,6 +33,12 @@ int main() {
     __half* half_host_A2;
     __half* half_host_B2;
     __half* half_res2;
+    __half* mixed_host_A1;
+    __half* mixed_host_B1;
+    float* mixed_res1;
+    __half* mixed_host_A2;
+    __half* mixed_host_B2;
+    float* mixed_res2;
 
     // Device data
     float* full_A1;
@@ -47,6 +53,12 @@ int main() {
     __half* half_A2;
     __half* half_B2;
     __half* half_C2;
+    __half* mixed_A1;
+    __half* mixed_B1;
+    float* mixed_C1;
+    __half* mixed_A2;
+    __half* mixed_B2;
+    float* mixed_C2;
 
 
     // Compute on sm_61 with full precision
@@ -118,7 +130,7 @@ int main() {
     // Compute on sm_61 with half precision
 
     utils.recordStartTime();
-    // Initialize full sm_61
+    // Initialize half sm_61
     HANDLE_ERROR( cudaSetDevice( sm61_gpu_idx ) );
     cublasHandle_t half_handle1;
     BLAS_HANDLE_ERROR( cublasCreate( &half_handle1 ) );
@@ -167,9 +179,8 @@ int main() {
     std::cout << half_dim * half_dim * sizeof(__half) / 1000000 << " MB memory per array, ";
     std::cout << utils.timeDifference() << " seconds" << std::endl;
 
-    for(int i = 0; i < 10; ++i) {
-        std::cout << __half2float(half_res1[i]) << std::endl;
-//        assert(__half2float(half_res1[i]) == 4 * half_dim);
+    for(int i = 0; i < half_dim * half_dim; ++i) {
+        assert(__half2float(half_res1[i]) == -8192);
     }
 
 
@@ -181,6 +192,83 @@ int main() {
     HANDLE_ERROR( cudaFree( half_A1 ) );
     HANDLE_ERROR( cudaFree( half_B1 ) );
     HANDLE_ERROR( cudaFree( half_C1 ) );
+
+
+
+
+    // Compute on sm_61 with mixed precision
+
+    utils.recordStartTime();
+    // Initialize mixed sm_61
+    HANDLE_ERROR( cudaSetDevice( sm61_gpu_idx ) );
+    cublasHandle_t mixed_handle1;
+    BLAS_HANDLE_ERROR( cublasCreate( &mixed_handle1 ) );
+    HANDLE_ERROR( cudaHostAlloc( (void**)&mixed_host_A1, half_dim * half_dim * sizeof(__half), cudaHostAllocDefault ) );
+    HANDLE_ERROR( cudaHostAlloc( (void**)&mixed_host_B1, half_dim * half_dim * sizeof(__half), cudaHostAllocDefault ) );
+    HANDLE_ERROR( cudaHostAlloc( (void**)&mixed_res1, half_dim * half_dim * sizeof(float), cudaHostAllocDefault ) );
+    HANDLE_ERROR( cudaMalloc( (void**)&mixed_A1, half_dim * half_dim * sizeof(__half) ) );
+    HANDLE_ERROR( cudaMalloc( (void**)&mixed_B1, half_dim * half_dim * sizeof(__half) ) );
+    HANDLE_ERROR( cudaMalloc( (void**)&mixed_C1, half_dim * half_dim * sizeof(float) ) );
+
+    __half mixed_val1 = __float2half(2.0f);
+    __half mixed_nval1 = __float2half(-2.0f);
+    for(int i = 0; i < half_dim * half_dim; ++i) {
+        mixed_host_A1[i] = mixed_val1;
+        mixed_host_B1[i] = mixed_nval1;
+    }
+
+    HANDLE_ERROR( cudaMemcpy( mixed_A1, mixed_host_A1, half_dim * half_dim * sizeof(__half), cudaMemcpyHostToDevice ) );
+    HANDLE_ERROR( cudaMemcpy( mixed_B1, mixed_host_B1, half_dim * half_dim * sizeof(__half), cudaMemcpyHostToDevice ) );
+    HANDLE_ERROR( cudaDeviceSynchronize() );
+
+
+    // Compute mixed sm_61
+    BLAS_HANDLE_ERROR( cublasGemmEx(
+            mixed_handle1,
+            CUBLAS_OP_N,
+            CUBLAS_OP_N,
+            half_dim,
+            half_dim,
+            half_dim,
+            &half_alpha,
+            mixed_A1,
+            CUDA_R_16F,
+            half_dim,
+            mixed_B1,
+            CUDA_R_16F,
+            half_dim,
+            &half_beta,
+            mixed_C1,
+            CUDA_R_32F,
+            half_dim,
+            CUBLAS_COMPUTE_32F,
+            CUBLAS_GEMM_DEFAULT_TENSOR_OP
+    ) );
+    HANDLE_ERROR( cudaDeviceSynchronize() );
+    HANDLE_ERROR( cudaMemcpy( mixed_res1, mixed_C1, half_dim * half_dim * sizeof(float), cudaMemcpyDeviceToHost ) );
+    utils.recordStopTime();
+
+    std::cout << std::endl;
+    std::cout << "CUDA 6.1 16-32-bit GemmEx, " << half_dim * half_dim << " elements per array, ";
+    std::cout << half_dim * half_dim * sizeof(__half) / 1000000 << " and ";
+    std::cout << half_dim * half_dim * sizeof(float) << " MB memory for 16-/32-bit arrays, ";
+    std::cout << utils.timeDifference() << " seconds" << std::endl;
+
+    for(int i = 0; i < half_dim * half_dim; ++i) {
+        assert(mixed_res1[i] == -4 * half_dim);
+    }
+
+
+    // Free sm_61 mixed
+    BLAS_HANDLE_ERROR( cublasDestroy( mixed_handle1 ) );
+    HANDLE_ERROR( cudaFreeHost( mixed_host_A1 ) );
+    HANDLE_ERROR( cudaFreeHost( mixed_host_B1 ) );
+    HANDLE_ERROR( cudaFreeHost( mixed_res1 ) );
+    HANDLE_ERROR( cudaFree( mixed_A1 ) );
+    HANDLE_ERROR( cudaFree( mixed_B1 ) );
+    HANDLE_ERROR( cudaFree( mixed_C1 ) );
+
+
 
 
 
@@ -302,9 +390,9 @@ int main() {
     std::cout << half_dim * half_dim * sizeof(__half) / 1000000 << " MB memory per array, ";
     std::cout << utils.timeDifference() << " seconds" << std::endl;
 
-    for(int i = 0; i < 10; ++i) {
-        std::cout << __half2float(half_res2[i]) << std::endl;
-//        assert(__half2float(half_res1[i]) == 4 * half_dim);
+    // CUDA __half has numerical limits (minimum and maximum values) of +/- 8192
+    for(int i = 0; i < half_dim * half_dim; ++i) {
+        assert(__half2float(half_res1[i]) == -8192);
     }
 
 
